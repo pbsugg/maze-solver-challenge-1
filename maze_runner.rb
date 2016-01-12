49b386e94dda7c24aae533cd8af0a634c8382bd1
Philip Sugg <pbensugg@gmail.com>
2016-01-12 09:21:17 -0600

    add initial try at movement--no tests yet--NEXT: will need a way to print board soon

--- maze_runner.rb
+++ maze_runner.rb
@@ -20,24 +20,34 @@ class MazeRunner
     @start_horizontal = maze_object.start_horizontal
   end
 
+
+
   # all the possible movements: look right, down, left, up
   # either update current position or give new position
   # getting vertical and horizontal coordinates w/i the maze
   # have these "if" checks b/c need to protect against wrap-around in array
   def check_up(vertical, horizontal)
-    @maze[vertical - 1][horizontal] if (vertical - 1) >= 0
+    space = @maze[vertical - 1][horizontal] if (vertical - 1) >= 0
+    return true if space == "."
+    false
   end
 
   def check_right(vertical, horizontal)
-    @maze[vertical][horizontal + 1] if (horizontal + 1) >= 0
+    space = @maze[vertical][horizontal + 1] if (horizontal + 1) >= 0
+    return true if space == "."
+    false
   end
 
   def check_down(vertical, horizontal)
-    @maze[vertical + 1][horizontal] if (vertical + 1) >= 0
+    space = @maze[vertical + 1][horizontal] if (vertical + 1) >= 0
+    return true if space == "."
+    false
   end
 
   def check_left(vertical, horizontal)
-    @maze[vertical][horizontal - 1] if (horizontal - 1) >= 0
+    space = @maze[vertical][horizontal - 1] if (horizontal - 1) >= 0
+    return true if space == "."
+    false
   end
 
 
@@ -80,6 +90,27 @@ class MazeRunner
     else
       [vertical, horizontal]
     end
+  end
+
+  # general logic for movement:
+  # overall DF movement algorithm
+  # check for movement in right, down, left, up order
+  # IF successful (success = return different position than current position)
+  # repeat movement algorithm
+  # ELSE (not successful)
+  # return nil or something similar
+
+  # this will move one space if possible
+  # input: current space
+  # output: new space OR nil if no movement possible
+  def move(vertical, horizontal)
+    return move_right(vertical, horizontal) if check_right(vertical, horizontal)
+    return move_down(vertical, horizontal) if check_down(vertical, horizontal)
+    return move_left(vertical, horizontal) if check_left(vertical, horizontal)
+    return move_up(vertical, horizontal) if check_up(vertical, horizontal)
+  end
+
+  def df_solve(vertical, horizontal)
   end
 
 end
